86654e740ed1fbfee5d0d8cb99a215104a3de7f0
devsjc <47188100+devsjc@users.noreply.github.com>
2023-03-29 14:41:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 import Reveal from 'reveal.js'
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 import Notes from 'reveal.js/plugin/notes/notes.esm.js'
+import Highlight from "reveal.js/plugin/highlight/highlight.esm.js";
 
 import 'reveal.js/dist/reveal.css'
 import 'reveal.js/dist/theme/sky.css'
-
+import 'reveal.js/plugin/highlight/monokai.css'
 
 let deck = new Reveal({
-  plugins: [ Markdown, Notes ]
+    plugins: [ Markdown, Notes, Highlight ]
 })
-deck.initialize({ hash: true, slideNumber: true })
+deck.initialize({
+    hash: true,
+    slideNumber: true,
+    backgroundTransition: "fade"
+})
